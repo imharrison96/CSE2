c1d02702f23c1609f7b0a7e15fe4c585db4e1944
imharrison96 <yinhaocheng2010@gmail.com>
2015-09-01 18:51:29 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,19 @@
+    public class WelcomeClass{
+         public static void main (String[ ] args) 
+         {
+             
+             System.out.println("  -----------");
+             System.out.println("  | WELCOME |");
+             System.out.println("  -----------");
+             System.out.println("  ^  ^  ^  ^  ^  ^");
+             System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+             System.out.println("<-H--A--Y--4--1--9->");
+             System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+             System.out.println("  v  v  v  v  v  v");
+             System.out.println("I went to Japan this summer with my friends.");
+             
+         }
+        
+        
+    
+}
